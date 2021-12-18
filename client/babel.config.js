9cb4378924e447e6a0c9ada52125ff7581c148a3
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2021-12-18 21:31:46 +0900

    バンドルサイズの削減
    対応するバージョンをchromeの過去2バージョンだけに
    使ってないpolyfillはバンドルしない

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,18 +1,21 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        corejs: '3',
-        modules: 'commonjs',
-        useBuiltIns: false,
-      },
+module.exports = (api) => {
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          corejs: '3',
+          modules: 'commonjs',
+          useBuiltIns: 'usage',
+          targets: ['last 2 Chrome versions'],
+        },
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: api.env('development'),
+        },
+      ],
     ],
-    [
-      '@babel/preset-react',
-      {
-        development: true,
-      },
-    ],
-  ],
+  };
 };
