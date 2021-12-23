ff1ad671369a99d3786ee2b32999f4716274a856
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2021-12-24 00:23:33 +0900

    Revert "vercelで動作するように修正"
    vercelを使わないことにしたので
    This reverts commit 805da79f16eb9e9a205933e19767396a60020766.
    
    Revert "vercelで動作するように修正"
    
    This reverts commit 98f356ca662bd78bdc9a19b9b2ff0b1e4542315a.
    
    Revert "vercelで動作するように修正"
    
    This reverts commit 1fc73daf19796a149537d95ffa204d18f207a809.

--- server/src/paths.js
+++ server/src/paths.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 const PUBLIC_PATH = path.resolve(__dirname, '../../public');
 const UPLOAD_PATH = path.resolve(__dirname, '../../upload');
-const CLIENT_DIST_PATH = path.resolve(__dirname, '../../client/dist');
+const CLIENT_DIST_PATH = path.resolve(__dirname, '../../dist');
 const DATABASE_PATH = path.resolve(__dirname, '../database.sqlite');
 
 export { PUBLIC_PATH, CLIENT_DIST_PATH, DATABASE_PATH, UPLOAD_PATH };
