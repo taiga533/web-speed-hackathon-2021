c8015c1f85a4b171a2d0ba2676b8950d71c40cc1
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2021-12-28 11:21:49 +0900

    画像読み込みの最適化
    画像を大と小に分ける

--- server/src/convert_all_image.js
+++ server/src/convert_all_image.js
@@ -1,22 +1,22 @@
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
-import { IMAGE_EXTENSION, IMAGE_FIT_STRATEGY, IMAGE_SIZE } from './image_const';
+import { IMAGE_EXTENSION, LARGE_IMAGE_SIZE, SMALL_IMAGE_SIZE } from './image_const';
 import { PUBLIC_PATH } from './paths';
 import { getFiles } from './utils/file_utils';
 
 const imagesPath = path.join(PUBLIC_PATH, '/images');
 
 async function convertAllImage() {
-  const imagePaths = getFiles(imagesPath);
+  const imagePaths = getFiles(imagesPath, false);
   await Promise.all(
     imagePaths
-      .filter((path) => path.match(/.jpe?g$/))
-      .map((path) => {
-        (async (imagePath) => {
-          const imageBuffer = await convertImage(imagePath);
-          saveConvertedFile(imagePath, imageBuffer);
-        })(path);
+      .filter((path) => path.match(/.webp$/))
+      .map(async (imagePath) => {
+        const largeImageBuffer = await convertImage(imagePath, 'large');
+        saveConvertedFile(imagePath, largeImageBuffer, 'large');
+        const smallImageBuffer = await convertImage(imagePath, 'small');
+        saveConvertedFile(imagePath, smallImageBuffer, 'small');
       }),
   );
 }
@@ -24,27 +24,30 @@ async function convertAllImage() {
 /**
  * 画像ファイルを変換
  * @param {string} imagePath
+ * @param {"large" | "small"} sizeName
  * @returns {Promise<Buffer>}
  */
-async function convertImage(imagePath) {
-  return sharp(imagePath)
-    .resize({
-      fit: IMAGE_FIT_STRATEGY,
-      height: IMAGE_SIZE.height,
-      width: IMAGE_SIZE.width,
-    })
-    .toFormat(IMAGE_EXTENSION)
-    .toBuffer();
+async function convertImage(imagePath, sizeName) {
+  return (
+    sharp(imagePath)
+      .resize({
+        position: 'center',
+        ...(sizeName === 'large' ? LARGE_IMAGE_SIZE : SMALL_IMAGE_SIZE),
+      })
+      // .toFormat(IMAGE_EXTENSION)
+      .toBuffer()
+  );
 }
 
 /**
  * 変換した画像を保存
  * @param {string} imagePath
  * @param {Buffer} imageBuffer
+ * @param {"large" | "small"} sizeName
  * @returns {Promise<void>}
  */
-async function saveConvertedFile(imagePath, imageBuffer) {
-  fs.promises.writeFile(imagePath.replace(/\.[a-z]+$/, `.${IMAGE_EXTENSION}`), imageBuffer);
+async function saveConvertedFile(imagePath, imageBuffer, sizeName) {
+  fs.promises.writeFile(imagePath.replace(/(-[a-zA-Z]+)?\.[a-z]+$/, `-${sizeName}.${IMAGE_EXTENSION}`), imageBuffer);
 }
 
 // 実行完了を待たない
--- server/src/image_const.js
+++ server/src/image_const.js
@@ -1,8 +1,16 @@
-const IMAGE_SIZE = {
-  width: 640,
-  height: 480,
+const LARGE_IMAGE_SIZE = {
+  width: 500,
 };
+const SMALL_IMAGE_SIZE = {
+  width: 300,
+};
+// const LARGE_IMAGE_SIZE = {
+//   width: 70,
+// };
+// const SMALL_IMAGE_SIZE = {
+//   width: 50,
+// };
 const IMAGE_EXTENSION = 'webp';
 const IMAGE_FIT_STRATEGY = 'outside';
 
-export { IMAGE_SIZE, IMAGE_EXTENSION, IMAGE_FIT_STRATEGY };
+export { LARGE_IMAGE_SIZE, SMALL_IMAGE_SIZE, IMAGE_EXTENSION, IMAGE_FIT_STRATEGY };
